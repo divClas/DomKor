11456ca9d03736c386a8ea4b6377cc699f32eb9a
Marat <marat.bagautdinov522@mail.ru>
2024-10-30 21:24:34 +0300

    Forms

--- src/store/instance.ts
+++ src/store/instance.ts
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import {RcFile} from "antd/es/upload";
 
 export const daDataFetch = (val: string) =>
     axios.post<{
@@ -18,7 +19,30 @@ export const daDataFetch = (val: string) =>
             "Authorization": "Token " + import.meta.env.VITE_API_KEY_DADATA
         }
     })
-
+export interface I_PayloadFile<I_Entity> {
+    ID: number
+    data: File | string | Blob | RcFile
+    key: (keyof I_Entity)
+}
+export async function uploadFile<T>(path: string, file: I_PayloadFile<T>['data'], name: (keyof T), tkn = AuthToken()): Promise<I_Response<T>> {
+    try {
+        const formData = new FormData()
+        formData.append(String(name), file)
+        const res = await axios.post<I_Response<T>>(import.meta.env.VITE_API_HOST + import.meta.env.VITE_API_URL + path, formData, {
+            headers: {
+                "Authorization": import.meta.env.VITE_API_ACCESS_TOKEN + '::' + tkn,
+                "Content-Type": "multipart/form-data"
+            }
+        });
+        return res.data
+    } catch (error: { message: string } | unknown) {
+        return {
+            error: true,
+            message: 'Ошибка сервера',
+            data: [] as I_Error[]
+        }
+    }
+}
 export default () =>
     axios.create({
         baseURL: import.meta.env.VITE_API_HOST + import.meta.env.VITE_API_URL,
