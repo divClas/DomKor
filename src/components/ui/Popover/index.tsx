6a4a39e35cf2d6bcb73ddaaae3d03f3bc7f4122a
Азат Фарваев <afarvaev4@gmail.com>
2024-11-01 17:12:45 +0300

    функция для отслежования состояния поповера

--- src/components/ui/Popover/index.tsx
+++ src/components/ui/Popover/index.tsx
@@ -1,33 +1,34 @@
-import {FC, ReactElement, ReactNode} from "react";
-import './style.scss'
-import {T_Background} from "@/types/app.ts";
-import {Button, Popover} from "antd";
+import { FC, ReactElement, ReactNode } from "react";
+import "./style.scss";
+import { T_Background } from "@/types/app.ts";
+import { Button, Popover } from "antd";
 
 export const PopoverWidget: FC<{
-    label: string
-    title: string
-    background: T_Background
-    icon?: ReactElement
-    onClick?: () => void
-    className?: string
-    type?: "button" | "submit" | "reset"
-    disabled?: boolean
-    children: ReactNode
+  label: string;
+  title: string;
+  background: T_Background;
+  icon?: ReactElement;
+  onClick?: () => void;
+  className?: string;
+  type?: "button" | "submit" | "reset";
+  disabled?: boolean;
+  children: ReactNode;
+  onOpenChange?: (visible: boolean) => void; // Add this prop
 }> = (props) => {
-    return (
-
-        <Popover
-            content={props.children}
-            title={props.title}
-            trigger={"click"}
-            forceRender={true}
-        >
-            <Button
-                children={props.label}
-                icon={props.icon}
-                className={props.className}
-                disabled={props.disabled}
-            />
-        </Popover>
-    )
-}
+  return (
+    <Popover
+      content={props.children}
+      title={props.title}
+      trigger={"click"}
+      forceRender={true}
+      onOpenChange={props.onOpenChange} // Pass it here
+    >
+      <Button
+        children={props.label}
+        icon={props.icon}
+        className={props.className}
+        disabled={props.disabled}
+      />
+    </Popover>
+  );
+};
--- src/components/widgets/GraphicsKpTable/GraphicKpListWidget.tsx
+++ src/components/widgets/GraphicsKpTable/GraphicKpListWidget.tsx
@@ -1,84 +1,85 @@
-import {ReactComponent as GetDownloadDoc} from "@/assets/getDownloadDoc.svg";
+import { ReactComponent as GetDownloadDoc } from "@/assets/getDownloadDoc.svg";
 import dayjs from "dayjs";
 import "dayjs/locale/ru";
-import {useAppSelector} from "@/hooks/storeHooks.ts";
-import {PopoverWidget} from "@/components/ui/Popover";
-import {Dictionary} from "@/contexts/Dictionary.ts";
-import {SubscribeKPForm} from "@/components/widgets/Forms/SubscribeKPForm.tsx";
-import {NoData} from "@/components/ui/NoData";
-import {FC} from "react";
-
+import { useAppSelector } from "@/hooks/storeHooks.ts";
+import { PopoverWidget } from "@/components/ui/Popover";
+import { Dictionary } from "@/contexts/Dictionary.ts";
+import { SubscribeKPForm } from "@/components/widgets/Forms/SubscribeKPForm.tsx";
+import { NoData } from "@/components/ui/NoData";
+import { FC, useState } from "react";
 
 export const GraphicKpListWidget: FC<{
-    onReset?: () => void
-}> = ({onReset}) => {
-    const {entity: graphicKpList, status} = useAppSelector((s) => s.graphicKP);
-    if (graphicKpList.length === 0 && status !== 'pending') {
-        return <NoData onReset={onReset}
-        />
-    }
-    return (
-        <>
-            {graphicKpList.map((item) => (
-                <div key={item.ID}
-                     className="graphics-container"
-                >
-                    <div className="first-column">
-                        <div className="kp-name">{item.NAME}</div>
-                        <a
-                            target="_blank"
-                            href={item.DOCUMENT}
-                            className="downloadDocument"
-                        >
-                            <GetDownloadDoc />
-                            <span>Скачать тендерную документацию</span>
-                        </a>
-                    </div>
-                    <div className="second-column">
-                        <div className="kp-char">
-                            <div className="kp-items">
-                                <div className="kp-item-title">Заявки принимаются до:</div>
-                                <div className="kp-item-description">
-                                    {dayjs(item.SUBMISSION_DEADLINE)
-                                        .locale("ru")
-                                        .format("D MMMM YYYY")}
-                                </div>
-                            </div>
-                            <div className="kp-items">
-                                <div className="kp-item-title">Дата окончания тендера:</div>
-                                <div className="kp-item-description">
-                                    {dayjs(item.TENDER_END_DATE)
-                                        .locale("ru")
-                                        .format("D MMMM YYYY")}
-                                </div>
-                            </div>
-                            <div className="kp-items">
-                                <div className="kp-item-title">Адрес:</div>
-                                <div className="kp-item-description">{item.ADDRESS}</div>
-                            </div>
-                            <div className="kp-items">
-                                <div className="kp-item-title">Ответственный:</div>
-                                <div className="kp-item-description">{item.PERSON}</div>
-                            </div>
-                        </div>
-                        <div className="organization">{item.LEGAL_ENTITY}</div>
-                    </div>
-                    <div className="third-column">
+  onReset?: () => void;
+}> = ({ onReset }) => {
+  const { entity: graphicKpList, status } = useAppSelector((s) => s.graphicKP);
+  if (graphicKpList.length === 0 && status !== "pending") {
+    return <NoData onReset={onReset} />;
+  }
+  const [activeId, setActiveId] = useState<string | null>(null);
 
-                        <PopoverWidget
-                            label={Dictionary.SEND_EVENT.ru}
-                            background={"accent"}
-                            title={Dictionary.SEND_EVENT_TENDER.ru}
-                            children={<SubscribeKPForm
-                                tender_id={item.ID}
-                            />}
-                        />
-                        <div className="date-bottom">
-                            {dayjs(item.DATE_CREATE).locale("ru").format("D MMMM YYYY")}
-                        </div>
-                    </div>
+  return (
+    <>
+      {graphicKpList.map((item) => (
+        <div
+          key={item.ID}
+          className={`graphics-container ${
+            activeId === item.ID ? "active-container" : ""
+          }`}
+        >
+          <div className="first-column">
+            <div className="kp-name">{item.NAME}</div>
+            <a
+              target="_blank"
+              href={item.DOCUMENT}
+              className="downloadDocument"
+            >
+              <GetDownloadDoc />
+              <span>Скачать тендерную документацию</span>
+            </a>
+          </div>
+          <div className="second-column">
+            <div className="kp-char">
+              <div className="kp-items">
+                <div className="kp-item-title">Заявки принимаются до:</div>
+                <div className="kp-item-description">
+                  {dayjs(item.SUBMISSION_DEADLINE)
+                    .locale("ru")
+                    .format("D MMMM YYYY")}
                 </div>
-            ))}
-        </>
-    );
+              </div>
+              <div className="kp-items">
+                <div className="kp-item-title">Дата окончания тендера:</div>
+                <div className="kp-item-description">
+                  {dayjs(item.TENDER_END_DATE)
+                    .locale("ru")
+                    .format("D MMMM YYYY")}
+                </div>
+              </div>
+              <div className="kp-items">
+                <div className="kp-item-title">Адрес:</div>
+                <div className="kp-item-description">{item.ADDRESS}</div>
+              </div>
+              <div className="kp-items">
+                <div className="kp-item-title">Ответственный:</div>
+                <div className="kp-item-description">{item.PERSON}</div>
+              </div>
+            </div>
+            <div className="organization">{item.LEGAL_ENTITY}</div>
+          </div>
+          <div className="third-column">
+            <PopoverWidget
+              label={Dictionary.SEND_EVENT.ru}
+              background={"accent"}
+              title={Dictionary.SEND_EVENT_TENDER.ru}
+              onOpenChange={(visible) => setActiveId(visible ? item.ID : null)}
+              children={<SubscribeKPForm tender_id={item.ID} />}
+            />
+            <div className="date-bottom">
+              {dayjs(item.DATE_CREATE).locale("ru").format("D MMMM YYYY")}
+            </div>
+          </div>
+        </div>
+      ))}
+    </>
+  );
 };
