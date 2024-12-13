a1d9ba838e87f210d45b1e19aa0d607365d72460
MaratBagautdinov <marat.bagautdinov522@mail.ru>
2024-12-13 10:28:22 +0300

    tabs

--- src/components/widgets/TableTabs/index.tsx
+++ src/components/widgets/TableTabs/index.tsx
@@ -5,18 +5,17 @@ import {PopoverWidget} from "@/components/ui/Popover";
 import {FormWidget} from "@/components/widgets/Form";
 import {FormSubscribeNotification} from "@/contexts/forms.ts";
 import useSizeHook from "@/hooks/useSizeHook.ts";
-import {GraphicTendersPage} from "@/components/pages/GraphicTenders/ui";
 import {TendersPage} from "@/components/pages/Tenders/ui";
 import {C_MB} from "@/contexts/theme.ts";
 
 export const TableTabsWidget = () => {
   const windowSize = useSizeHook()
   const items: TabsProps["items"] = [
-    {
-      key: Dictionary.GRAPHIC.en,
-      label: windowSize.width > C_MB ? Dictionary.GRAPHIC_TAB.ru : Dictionary.GRAPHIC_TAB_MB.ru,
-      children: <GraphicTendersPage />,
-    },
+    // {
+    //   key: Dictionary.GRAPHIC.en,
+    //   label: windowSize.width > C_MB ? Dictionary.GRAPHIC_TAB.ru : Dictionary.GRAPHIC_TAB_MB.ru,
+    //   children: <GraphicTendersPage />,
+    // },
     {
       key: Dictionary.GRAPHIC_KP.en,
       label: windowSize.width > C_MB ? Dictionary.GRAPHIC_KP_TAB.ru : Dictionary.GRAPHIC_KP_TAB_MB.ru,
@@ -25,7 +24,7 @@ export const TableTabsWidget = () => {
   ];
   return (
     <Tabs
-      defaultActiveKey={Dictionary.GRAPHIC.en}
+      defaultActiveKey={Dictionary.GRAPHIC_KP_TAB.en}
       items={items}
       tabBarExtraContent={
         <>
